60e96c70caf5a7d469115c0b0587a4d37ed12a43
Tobi406 <tobim406@gmail.com>
2020-06-05 20:17:24 +0200

    Add pages

---
+++ pages/wiki.vue
@@ -0,0 +1,190 @@
+<template>
+  <main id="wiki" class="wiki container">
+    <article id="article">
+      <nuxt-child />
+    </article>
+    <aside>
+      <h1>Wiki</h1>
+      <div v-html="sidebar" />
+    </aside>
+  </main>
+</template>
+
+<script>
+import Sidebar from '~/assets/data/wiki/_Sidebar.md';
+
+export default {
+  middleware ({route, redirect}) {
+    if (route.path === '/wiki') redirect('/wiki/Home');
+  },
+  metaInfo: {
+    titleTemplate: '%s | Wiki | LuckPerms',
+  },
+  mounted() {
+    if (window.innerWidth >= 922) return;
+    const wiki = document.getElementById('wiki');
+    document.querySelectorAll('.wiki aside a').forEach((link) => {
+      link.addEventListener('click', () => {
+        wiki.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      });
+    });
+  },
+  computed: {
+    sidebar() {
+      return Sidebar;
+    },
+  }
+};
+</script>
+
+<style lang="scss">
+  .wiki {
+    display: flex;
+    align-items: flex-start;
+    background-color: $grey;
+    height: 100%;
+    flex-wrap: wrap;
+    overflow-y: scroll;
+    @include breakpoint($md) {
+      overflow-y: auto;
+    }
+    .header-anchor {
+      text-decoration: none;
+      display: none;
+    }
+  }
+  aside {
+    width: 100%;
+    padding: 2em 2em 0 2em;
+    background-color: rgba(0,0,0,.25);
+    order: 2;
+    @include breakpoint($md) {
+      flex: 2;
+      order: 1;
+      overflow: auto;
+      height: 100%;
+      max-width: 20rem;
+    }
+    @include breakpoint($lg) {
+      flex: 0 0 24em;
+    }
+    section {
+      margin-bottom: 2rem;
+    }
+    h1 {
+      font-size: 2rem;
+      margin-top: 0;
+    }
+    a {
+      text-decoration: none;
+      margin: .2rem;
+    }
+    ul {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      ul {
+        padding-left: 1rem;
+      }
+    }
+  }
+  #article {
+    flex: 3;
+    max-width: 100%;
+    position: relative;
+    width: 100%;
+    order: 1;
+    @include breakpoint($md) {
+      height: 100%;
+      overflow: auto;
+      order: 2;
+    }
+    h1 {
+      background-color: rgba(0,0,0,.5);
+      font-size: 4rem;
+      padding: 1rem 2rem;
+      margin: 0;
+    }
+    section {
+      width: 100%;
+      padding: 0;
+      @include breakpoint($md) {
+        position: absolute;
+      }
+      h1, h2 {
+        background: rgba(0,0,0,.25);
+        padding: 1rem 2rem;
+        font-size: 2rem;
+        margin-top: 0;
+      }
+      h3, h4, h5, h6, p, ol, ul {
+        padding: 0 2rem;
+      }
+      hr {
+        border-color: rgba(0,0,0,.25);
+      }
+      a {
+        text-decoration: none;
+      }
+      p, ol, ul {
+        color: rgba(255,255,255,.66);
+        code {
+          color: #FFF;
+        }
+        a {
+          > code {
+            color: $brand-color;
+          }
+        }
+      }
+      pre {
+        padding: 0 2rem;
+        white-space: pre-wrap;
+        code {
+          padding: 1.5rem 2rem;
+          border: 1px solid rgba(0,0,0,.2);
+        }
+      }
+      table {
+        margin: 1rem 2rem;
+        background: rgba(0,0,0,.2);
+        border: 1px solid rgba(0,0,0,.2);
+        border-collapse: collapse;
+        tr {
+          border-top: 1px solid rgba(0,0,0,.2);
+        }
+        td, th {
+          padding: .5rem 1rem;
+          border: 1px solid rgba(0,0,0,.2);
+        }
+        th {
+          background: rgba(0,0,0,.2);
+        }
+      }
+      blockquote {
+        margin: 2rem 2rem;
+        padding: .01rem 0;
+        border-left: .5rem solid rgba(0,0,0,.25);
+        background: rgba(0,0,0,.1);
+      }
+      img {
+        max-width: 100%;
+        &:not([src$="png"]) {
+          box-shadow: 0 0 1rem rgba(0,0,0,.2);
+        }
+      }
+      h2:hover > .header-anchor,
+      h3:hover > .header-anchor {
+        display: initial;
+      }
+    }
+  }
+  :target {
+    background: $brand-color;
+    color: #FFF;
+    border: 1px red solid;
+  }
+</style>
---
+++ pages/wiki/_page.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+      <h1>
+        <transition name="fade" mode="out-in">
+          <span :key="title">
+            {{ title }}
+          </span>
+        </transition>
+      </h1>
+      <transition name="fade" mode="out-in">
+        <div v-html="article" />
+      </transition>
+    </div>
+
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    const { title } = this;
+    return {
+      title,
+    };
+  },
+  data() {
+    return {
+      article: null,
+    };
+  },
+  computed: {
+    route() {
+      return this.$route.params.page;
+    },
+    title() {
+      return this.route.split('-').join(' ');
+    },
+  },
+  created() {
+    if (this.route) {
+      this.getArticle();
+    }
+  },
+  methods: {
+    async getArticle() {
+      if (process.client) {
+        // eslint-disable-next-line global-require,import/no-dynamic-require
+        this.article = require(`~/assets/data/wiki/${this.route}.md`).default;
+        await this.$nextTick();
+        document.querySelectorAll('.wiki a').forEach((link) => {
+          link.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target;
+            if (['STRONG', 'CODE'].includes(event.target.tagName)) {
+              target = event.target.parentNode;
+            } else if (event.target.tagName === 'A') {
+              ({ target } = event);
+            }
+            if (target.href.startsWith(window.origin)) {
+              this.$router.push({
+                path: target.pathname,
+                hash: target.hash,
+              }).then().catch(() => {});
+            } else {
+              window.open(target.href);
+            }
+          });
+        });
+        if (this.$route.hash) {
+          console.log(this.$route.hash);
+          await this.scrollTo(this.$route.hash);
+        }
+      }
+    },
+    async scrollTo(hash) {
+      if (process.client) {
+        await this.$nextTick();
+        const element = document.getElementById(hash.split('#')[1]);
+        console.log(element);
+        if (!element) return;
+        element.scrollIntoView({
+          behavior: 'smooth',
+        });
+      }
+    },
+  },
+  watch: {
+    $route(to) {
+      if (process.client && to.hash) {
+        this.$nextTick().then(() => {
+          this.scrollTo(to.hash);
+        });
+      } else {
+        document.getElementById('article').scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }
+      this.getArticle();
+    },
+  },
+};
+</script>

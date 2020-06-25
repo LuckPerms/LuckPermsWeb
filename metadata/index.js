270e91bb264329551cd67d79850e1ae012ba1b86
Tobi406 <tobim406@gmail.com>
2020-06-25 15:05:27 +0200

    Serve _Sidebar and Home pages

--- metadata/index.js
+++ metadata/index.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
+const data = {
+  wiki: {},
+};
+const wikiPages = [
+  'Home',
+  '_Sidebar',
+];
+
+getData().then(() => {
+  setInterval(async () => {
+    await getWikiData();
+  }, 30000);
+});
+
+async function getData() {
+  await getWikiData();
+};
+
+async function getWikiData() {
+  try {
+    wikiPages.forEach(async (page) => {
+      const result = await axios.get(`https://raw.githubusercontent.com/LuckPerms/wiki/master/${page}.md`);
+      data.wiki[page] = result.data;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 app.get('/wiki/:page', (req, res) => {
-  res.json(req.params);
+  res.json({page: data.wiki[req.params['page']]});
 });
 
 module.exports = {

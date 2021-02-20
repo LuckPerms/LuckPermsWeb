41dd7351f81c6301c736419df9b46571f19d8a5e
Luck <git@lucko.me>
2021-02-20 13:34:34 +0000

    oops fix intent size

--- src/util/axios_compress.js
+++ src/util/axios_compress.js
@@ -3,10 +3,10 @@ import pako from 'pako';
 
 // Returns an axios "config" object with a custom transformRequest for gzip.
 export default Object.assign({
-    transformRequest: axios.defaults.transformRequest.concat(
-        function (data, headers) {
-            headers['Content-Encoding'] = 'gzip';
-            return pako.gzip(data);
-        },
-    ),
+  transformRequest: axios.defaults.transformRequest.concat(
+    function (data, headers) {
+      headers['Content-Encoding'] = 'gzip';
+      return pako.gzip(data);
+    },
+  ),
 })

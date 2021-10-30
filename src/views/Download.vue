929bc472af4c4d5816de05104a730b2deba42fd6
Tobi406 <tobim406@gmail.com>
2021-10-30 13:23:10 +0200

    Use i18n locale instead of local lang for date

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -308,7 +308,7 @@ export default {
     versionTimestamp() { return this.$store.getters.versionTimestamp; },
     relativeTimestamp() {
       if (this.versionTimestamp) {
-        return relativeDate(this.versionTimestamp, new Date().getTime(), true);
+        return relativeDate(this.versionTimestamp, this.$i18n.locale, new Date().getTime(), true);
       }
       return null;
     },
@@ -326,7 +326,7 @@ export default {
       this.quiz.open = false;
     },
     relativeDate(value) {
-      return relativeDate(value);
+      return relativeDate(value, this.$i18n.locale);
     },
   },
 };

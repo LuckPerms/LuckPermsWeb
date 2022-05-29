685aeb0b13fc0b824f49af55d4febb3b9add6292
Herr Erde <Natodamm@hotmail.com>
2022-05-29 19:43:23 +0200

    removed other versions

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -27,77 +27,6 @@
           <a :href="downloads.velocity" v-on:click="logDownload('velocity')" class="resource">
             <span>
               <img src="@/assets/logos/velocity.png" alt="Velocity" />
-          <a
-            :href="downloads.sponge"
-            v-on:click="logDownload('sponge')"
-            class="resource"
-          >
-            <span>
-              <img src="@/assets/logos/sponge.png" alt="Sponge">
-              Sponge
-            </span>
-            <small>{{ $t('download.sponge') }}</small>
-          </a>
-          <a
-            :href="downloads.fabric"
-            v-on:click="logDownload('fabric')"
-            class="resource"
-          >
-            <span>
-              <img src="@/assets/logos/fabric.png" alt="Fabric">
-              Fabric
-            </span>
-            <small>{{ $t('download.fabric') }}</small>
-          </a>
-          <a
-            :href="downloads.forge"
-            v-on:click="logDownload('forge')"
-            class="resource"
-          >
-            <span>
-              <img src="@/assets/logos/forge.png" alt="Forge">
-              Forge
-            </span>
-            <small>{{ $t('download.forge') }}</small>
-          </a>
-          <a
-            :href="downloads.nukkit"
-            v-on:click="logDownload('nukkit')"
-            class="resource"
-          >
-            <span>
-              <img src="@/assets/logos/nukkit.png" alt="Nukkit">
-              Nukkit
-            </span>
-            <small>{{ $t('download.nukkit') }}</small>
-          </a>
-          <a
-            :href="downloads.velocity"
-            v-on:click="logDownload('velocity')"
-            class="resource"
-          >
-            <span>
-              <img src="@/assets/logos/velocity.png" alt="Velocity">
-              Velocity
-            </span>
-            <small>{{ $t("download.velocity") }}</small>
-          </a>
-          <a
-            :href="downloads.bungee"
-            v-on:click="logDownload('bungee')"
-            class="resource"
-          >
-            <span>
-              <img src="@/assets/logos/bungeecord.png" alt="BungeeCord">
-              BungeeCord
-            </span>
-            <small>{{ $t('download.bungee') }}</small>
-          </a>
-          <a
-            :href="downloads['bukkit-legacy']"
-            v-on:click="logDownload('bukkit-legacy')"
-            class="resource"
-          >
             <span>
               <img src="@/assets/logos/bukkit.png" alt="Bukkit">
               Bukkit Legacy
@@ -109,7 +38,6 @@
             {{ $t('download.typeHelp') }}
           </button>
         </div>
-
         <div>
           <h2>{{ $t("download.changelog") }}</h2>
           <ul class="changelog">
